e9959518f90f329b7980bc79a51790b484f321f9
Sanghamitra Ahirrao <71121214+sangha-dev-git@users.noreply.github.com>
2025-01-21 15:45:55 +0530

    Update test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,7 +1,31 @@
+# process1.process
+
+
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../scripts')))
 
+import importlib.util 
+cwd = os.getcwd()
+print('cwd : ' +str(cwd))
+
+parent_dir = os.path.dirname(cwd)
+
+abs_pipeline_path = str(parent_dir)+ '\scripts' + '\pipeline.py'
+print(abs_pipeline_path)
+
+
+spec = importlib.util.spec_from_file_location("pipeilne", abs_pipeline_path) 
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+my_module = module
+
+
+
+
+
+
+# my_module
 import unittest
 # from scripts.pipeline import process_data
 
@@ -10,6 +34,7 @@ from .scripts.pipeline import process_data
 class TestPipeline(unittest.TestCase):
     def test_process_data(self):
         # Add your test logic here
+        result = my_module.process_data() 
         print("Testing data processing...")
         self.assertTrue(True)
 
