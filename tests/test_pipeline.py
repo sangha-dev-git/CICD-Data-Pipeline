096c1dea67a8286c512d8c8aa659f9e231d8a741
Sanghamitra Ahirrao <71121214+sangha-dev-git@users.noreply.github.com>
2025-01-21 14:56:10 +0530

    Update test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -3,7 +3,9 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../scripts')))
 
 import unittest
-from scripts.pipeline import process_data
+# from scripts.pipeline import process_data
+
+from .scripts.pipeline import process_data
 
 class TestPipeline(unittest.TestCase):
     def test_process_data(self):
